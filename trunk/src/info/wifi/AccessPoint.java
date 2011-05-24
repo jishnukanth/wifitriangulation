edb696a567389c0b5afdd6eb7b6e8d69b3735e37
littledannyha@gmail.com <littledannyha@gmail.com@42641f0b-7ea2-2af2-ca9f-d55ac9e1007f>
2011-05-24 14:23:49 +0000

    Removed the essid methods and field

--- trunk/src/info/wifi/AccessPoint.java
+++ trunk/src/info/wifi/AccessPoint.java
@@ -3,21 +3,15 @@ package info.wifi;
 public class AccessPoint 
 {
 	private Location location;
-	private String essid;
 	private String bssid;
 	private int signal;
 	
-	public AccessPoint(String ess, String bss, int sig, Location loc)
+	public AccessPoint(String bss, int sig, Location loc)
 	{
 		location = loc;
-		essid = ess;
+
 		bssid = bss;
 		signal = sig;
-	}
-	
-	public String getEssid()
-	{
-	return essid;	
 	}
 	
 	public String getBssid()
