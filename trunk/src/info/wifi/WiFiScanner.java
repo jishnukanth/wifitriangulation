59980d41d35568d21157b222b3c859acedae6421
cunninghampatt@gmail.com <cunninghampatt@gmail.com@42641f0b-7ea2-2af2-ca9f-d55ac9e1007f>
2011-05-24 14:24:27 +0000

    Almost done parsing netsh command

--- trunk/src/info/wifi/WiFiScanner.java
+++ trunk/src/info/wifi/WiFiScanner.java
@@ -1,9 +1,86 @@
 package info.wifi;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class WiFiScanner 
 {
-	public WiFiScanner()
+	public static ArrayList<String> scan()
 	{
+		ArrayList<String> output = new ArrayList<String>();
 		
+		Process process = null;
+		
+		try
+		{
+			process = Runtime.getRuntime().exec("netsh wlan show networks mode=bssid");
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		InputStream out = process.getInputStream();
+		BufferedReader br = new BufferedReader(new InputStreamReader(out));
+		String line;
+		
+		try
+		{
+			while((line = br.readLine()) != null)
+			{
+				output.add(line);
+			}
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		return output;
+	}
+	
+	public static ArrayList<AccessPoint> parseAccessPoint(ArrayList<String> ap)
+	{
+		ArrayList<AccessPoint> output = new ArrayList<AccessPoint>();
+		ArrayList<String> bssid = new ArrayList<String>();
+		ArrayList<String> signalString = new ArrayList<String>();
+		ArrayList<Integer> signalInt = new ArrayList<Integer>();
+		
+		
+		for(String str : ap)
+		{
+			if(str.indexOf("BSSID") == -1 && str.indexOf("Signal") == -1)
+			{
+				ap.remove(str);
+			}
+		}
+		
+		for(String str : ap)
+		{
+			if(str.indexOf("BSSID") != -1)
+			{
+				bssid.add(str.substring(str.indexOf(":") + 2));
+			}
+			else if(str.indexOf("Signal") != -1)
+			{
+				signalString.add(str.substring(str.indexOf(":") + 2));
+			}
+		}
+		
+		for(String str : signalString)
+		{
+			str = str.substring(0,str.indexOf("%"));
+			signalInt.add(Integer.parseInt(str));
+		}
+		
+		for(int i = 0; i < bssid.size(); i++)
+		{
+			output.add(new AccessPoint(bssid.get(i),signalInt.get(i),new Location(-1000,-1000)));
+		}
+		
+		return output;
 	}
 }
