3a9f18bb312943d9187194b704179873c394cb59
littledannyha@gmail.com <littledannyha@gmail.com@42641f0b-7ea2-2af2-ca9f-d55ac9e1007f>
2011-05-24 14:22:26 +0000

    Wrote out the AccessPoint class and Location class

--- trunk/src/info/wifi/AccessPoint.java
+++ trunk/src/info/wifi/AccessPoint.java
@@ -2,5 +2,45 @@ package info.wifi;
 
 public class AccessPoint 
 {
+	private Location location;
+	private String essid;
+	private String bssid;
+	private int signal;
 	
+	public AccessPoint(String ess, String bss, int sig, Location loc)
+	{
+		location = loc;
+		essid = ess;
+		bssid = bss;
+		signal = sig;
+	}
+	
+	public String getEssid()
+	{
+	return essid;	
+	}
+	
+	public String getBssid()
+    {
+    	return bssid;
+    }
+    
+    public int getSignal()
+    {
+    	return signal;
+    }
+    
+    public Location getLocation()
+    {
+    	return location;
+    }
+    
+    public void setLocation(Location loc)
+    {
+    	location = loc;
+    }
+    
+    
 }
+
+
