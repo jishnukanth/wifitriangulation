0343a2d20175e345a6195cbca06bb461048aa269
littledannyha <littledannyha@gmail.com>
2011-05-24 14:34:05 +0000

    Modified AccessPoint and started work on GPSMatcher

--- src/info/wifi/GPSMatcher.java
+++ src/info/wifi/GPSMatcher.java
@@ -1,6 +1,30 @@
 package info.wifi;
 
+import java.util.ArrayList;
+
+
 public class GPSMatcher 
 {
+	private ArrayList<AccessPoint> currentAP;
+    private ArrayList<AccessPoint> masterAP;
+    
+	public GPSMatcher(ArrayList<AccessPoint> available, ArrayList<AccessPoint> masterList)
+	{
+		currentAP = available;
+		masterAP = masterList;
+	}
 
+//takes an access point from available and tries to match it to master list
+//it then fills in the location data from the master list into the current list
+public void fillInLocations()
+{
+	
+}
+
+//
+public Location determineLocation()
+{
+	
+}
+	
 }
