a6782fc59b9fad3e02fbc1c4efcb96fae6aeef78
littledannyha@gmail.com <littledannyha@gmail.com@42641f0b-7ea2-2af2-ca9f-d55ac9e1007f>
2011-05-27 14:40:30 +0000

    Attempted and failed to make Display class

--- trunk/src/info/wifi/Display.java
+++ trunk/src/info/wifi/Display.java
@@ -1,6 +1,19 @@
 package info.wifi;
 
-public class Display 
+import javax.swing.JFrame;
+
+public class Display extends JFrame
 {
+private Map m = null;
+
+public Display(Map m)
+{
+	
+}
+
+public void updateMap ()
+{
+	
+}
 
 }
